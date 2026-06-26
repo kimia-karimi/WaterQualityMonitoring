b92b6a6f3b312ab079ae7c502178c70f7c400939
kimia-karimi <kim.karimi@twdb.texas.gov>
2026-06-26 11:16:08 -0500

    Add hydrovu_targets.py with target configurations

---
+++ config/hydrovu_targets.py
@@ -0,0 +1,14 @@
+HYDROVU_TARGETS = [
+    {#ccbay
+        "id": 6387509350170624,
+        "name": "default-1031422",
+        "latitude": 40.58833,
+        "longitude": -105.06587
+    },
+    {#baffinbay
+        "id": 5106941840719872,
+        "name": "default-1031425",
+        "latitude": 27.267969131469727,
+        "longitude": -97.50414276123047
+    }
+]
