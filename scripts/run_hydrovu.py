fa991f524af5c32cc708fdbb7d1c086cfeee308a
kimia-karimi <kim.karimi@twdb.texas.gov>
2026-06-26 11:32:35 -0500

    Update run_hydrovu.py

--- scripts/run_hydrovu.py
+++ scripts/run_hydrovu.py
@@ -1,8 +1,16 @@
 import hydrovu_api
 from processing.payload_to_csv import rows_to_csv_payload
-
+CLIENT_ID= "TWDB"
+CLIENT_SECRET= "65af7106c92646bf9249d4e7d92f8f61"
 oauth = hydrovu_api.get_oauth_session(CLIENT_ID)
 hydrovu_api.get_access_token(oauth, CLIENT_ID, CLIENT_SECRET)
+
+results = run_hydrovu_for_targets(session, HYDROVU_TARGETS)
+
+# Print / save
+for r in results:
+    print(f"\n--- Station {r['id']} ---\n")
+    print(r["csv"].getvalue().decode("utf-8"))
 
 
 for site in HYDROVU_TARGETS:
