2d3de2e5c72034e63f7244c1027579902d4f2a09
kimia-karimi <kim.karimi@twdb.texas.gov>
2026-06-26 14:08:51 -0500

    upd

--- processing/payload_to_csv.py
+++ processing/payload_to_csv.py
@@ -49,7 +49,7 @@ def rows_to_csv_payload(payload, default_depth_m=None):
     for ts in sorted(rows_by_ts.keys()):
         writer.writerow([ts] + rows_by_ts[ts])
 
-    text.flush()
-    buff.seek(0)
+    #text.flush()
+    #buff.seek(0)
 
     return buff
