9c7e3367c480b221e2778df034b4dcb716e005e5
kimia-karimi <kim.karimi@twdb.texas.gov>
2026-06-26 13:14:04 -0500

    update modules

--- processing/hydrovu_runner.py
+++ processing/hydrovu_runner.py
@@ -1,5 +1,5 @@
-from APIs import hydrovu_api
-from processing.payload_to_csv import rows_to_csv_payload
+#from APIs.hydrovu_api import hydrovu_api
+#from processing.payload_to_csv import rows_to_csv_payload
 from APIs.hydrovu_api import get_timeseries_payload
 def run_hydrovu_for_targets(session, targets, start=None, end=None):
     results = []
--- scripts/run_hydrovu.py
+++ scripts/run_hydrovu.py
@@ -2,11 +2,12 @@ from config.hydrovu_targets import HYDROVU_TARGETS
 from processing.payload_to_csv import rows_to_csv_payload
 from processing.hydrovu_runner import run_hydrovu_for_targets
 from APIs.hydrovu_api import get_oauth_session
-
+from APIs.hydrovu_api import get_timeseries_payload
+from APIs.hydrovu_api import get_access_token
 CLIENT_ID= "TWDB"
 CLIENT_SECRET= "65af7106c92646bf9249d4e7d92f8f61"
-oauth = APIs.hydrovu_api.get_oauth_session(CLIENT_ID)
-APIs.hydrovu_api.get_access_token(oauth, CLIENT_ID, CLIENT_SECRET)
+oauth = get_oauth_session(CLIENT_ID)
+get_access_token(oauth, CLIENT_ID, CLIENT_SECRET)
 
 results = run_hydrovu_for_targets(oauth, HYDROVU_TARGETS)
 
@@ -17,7 +18,7 @@ for r in results:
 
 
 for site in HYDROVU_TARGETS:
-    payload = APIs.hydrovu_api.get_timeseries_payload(
+    payload = get_timeseries_payload(
         oauth,
         location_id=site["id"], 
         start_time=None,   # or "2025-01-01T00:00:00Z"
