9944f56f94fc359baaa6dc0d3daa82980d9714f7
kimia-karimi <kim.karimi@twdb.texas.gov>
2026-06-26 15:11:56 -0500

    upd

--- processing/hydrovu_runner.py
+++ processing/hydrovu_runner.py
@@ -1,10 +1,14 @@
 #from APIs.hydrovu_api import hydrovu_api
-#from processing.payload_to_csv import rows_to_csv_payload
+from processing.payload_to_csv import rows_to_csv_payload
 from APIs.hydrovu_api import get_timeseries_payload
 def run_hydrovu_for_targets(session, targets, start=None, end=None):
     results = []
 
     for site in targets:
+        location_id = site["id"]
+
+        print(f"Processing HydroVu location {location_id}")
+
         payload = get_timeseries_payload(
             session,
             location_id=site["id"],
