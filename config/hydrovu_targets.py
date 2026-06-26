e99ad827026a85b828ad764ae1c387c58a5911e7
kimia-karimi <kim.karimi@twdb.texas.gov>
2026-06-26 16:25:39 -0500

    upd

--- config/hydrovu_targets.py
+++ config/hydrovu_targets.py
@@ -1,13 +1,13 @@
 HYDROVU_TARGETS = [
     {#ccbay
         "id": 6387509350170624,
-        "name": "default-1031422",
+        "name": "default-1031422 CCBAY",
         "latitude": 40.58833,
         "longitude": -105.06587
     },
     {#baffinbay
         "id": 5106941840719872,
-        "name": "default-1031425",
+        "name": "default-1031425" BAFFINBAY",
         "latitude": 27.267969131469727,
         "longitude": -97.50414276123047
     }
