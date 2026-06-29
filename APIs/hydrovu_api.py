a3f850bab24c8939122096a0e2bcb2672bc6a888
kimia-karimi <kim.karimi@twdb.texas.gov>
2026-06-29 11:18:54 -0500

    ajust flatten_into_rows

--- APIs/hydrovu_api.py
+++ APIs/hydrovu_api.py
@@ -114,7 +114,7 @@ def flatten_into_rows(location_info, friendly_names):
     data_rows_by_timestamp = {}
     for parameter in location_info.get('parameters', []):
         parameter_id = parameter['parameterId']
-        for datum in parameter('readings',[]):
+        for datum in parameter.get('readings',[]):
             timestamp = datum['timestamp']
             if timestamp is None:
                 continue
@@ -122,7 +122,7 @@ def flatten_into_rows(location_info, friendly_names):
             if dt_str not in data_rows_by_timestamp:
                 data_rows_by_timestamp[dt_str] = [None for _ in parameter_col_index]
             col_info = parameter_col_index[parameter_id]
-            data_rows_by_timestamp[dt_str][col_info['columnIndex']] = datum['value']
+            data_rows_by_timestamp[dt_str][col_info['columnIndex']] = datum.get['value']
 
     return data_rows_by_timestamp, parameter_col_index
 
