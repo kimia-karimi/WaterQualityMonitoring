9fdb268768c0ea8f61782bcb5dd4b20e5965da44
kimia-karimi <kim.karimi@twdb.texas.gov>
2026-07-08 10:16:32 -0500

    fix logging error

--- scripts/run_daily_hydrovu.py
+++ scripts/run_daily_hydrovu.py
@@ -33,7 +33,6 @@ def main():
     # ✅ authenticate once
     session = get_oauth_session(CLIENT_ID)
     get_access_token(session, CLIENT_ID, CLIENT_SECRET)
-    logging.info(session.headers,CLIENT_ID, CLIENT_SECRET)
 
     # ✅ cache friendly names once
     friendly_names = fetch_friendly_names(session)
